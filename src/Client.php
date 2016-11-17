e94f66c702330d05666a49130089c6971c0d690c
Ivan Lemeshev <vanlmshv@gmail.com>
2016-11-17 13:57:13 +0600

    Add client and tests

---
+++ src/Client.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace IvanLemesehev\OnPBX;
+
+class Client
+{
+    /**
+     * Request method
+     */
+    const METHOD = 'POST';
+
+    /**
+     * OnPBX API domain
+     */
+    const API_DOMAIN = 'api.onlinepbx.ru';
+
+    /**
+     * Accept header
+     */
+    const HEADER_ACCEPT = 'application/json';
+
+    /**
+     * Content type header
+     */
+    const HEADER_CONTENT_TYPE = 'application/x-www-form-urlencoded';
+
+    /**
+     * @var string
+     */
+    private $domain;
+
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var string
+     */
+    private $authKey;
+
+    /**
+     * @var string
+     */
+    private $authKeyId;
+
+    /**
+     * Client constructor.
+     *
+     * @param string $domain
+     * @param string $apiKey
+     *
+     * @throws \Exception
+     */
+    public function __construct($domain, $apiKey)
+    {
+        $this->domain = $domain;
+        $this->apiKey = $apiKey;
+
+        $this->auth();
+    }
+
+    /**
+     * Make HTTP request and return a response.
+     *
+     * @param string $url
+     * @param array $body
+     *
+     * @return array|bool
+     */
+    public function get($url, $body = [])
+    {
+        $url = $this->getFullUrl($url);
+
+        if (is_array($body)) {
+            foreach ($body as $key => $value) {
+                if (is_string($key) && preg_match('/^@(.+)/', $value, $m)) {
+                    $body[$key] = [
+                        'name' => basename($m[1]),
+                        'data' => base64_encode(file_get_contents($m[1])),
+                    ];
+                }
+            }
+        }
+
+        $post = http_build_query($body);
+        $headers = $this->createHeaders($url, $post, self::METHOD);
+
+        return $this->makeRequest(self::METHOD, $url, $post, $headers);
+    }
+
+    /**
+     * Make authenticated HTTP requests and set API key and API key ID.
+     */
+    private function auth()
+    {
+        $url = $this->getFullUrl('/auth.json');
+
+        $data = ['auth_key' => $this->apiKey];
+
+        $response = $this->makeRequest(self::METHOD, $url, $data);
+
+        if ($response['comment'] == 'not authenticated') {
+            $data['new'] = 'true';
+            $response = $this->makeRequest(self::METHOD, $url, $data);
+        }
+
+        if (isset($response['data']['key']) && isset($response['data']['key_id'])) {
+            $this->authKey = $response['data']['key'];
+            $this->authKeyId = $response['data']['key_id'];
+        }
+    }
+
+    /**
+     * Make HTTP request.
+     *
+     * @param string $method
+     * @param string $url
+     * @param $post
+     * @param array $headers
+     *
+     * @return bool|mixed
+     */
+    private function makeRequest($method, $url, $post, $headers = [])
+    {
+        $ch = curl_init("https://{$url}");
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+
+        $response = json_decode(curl_exec($ch), true);
+
+        if ($response) {
+            return $response;
+        }
+
+        return false;
+    }
+
+    /**
+     * Generate full URL.
+     *
+     * @param string $url
+     *
+     * @return string
+     */
+    private function getFullUrl($url)
+    {
+        return self::API_DOMAIN . '/' . $this->domain . $url;
+    }
+
+    /**
+     * Create HTTP request headers.
+     *
+     * @param string $url
+     * @param string $post
+     * @param string $method
+     *
+     * @return array
+     */
+    private function createHeaders($url, $post, $method)
+    {
+        $date = date('r');
+        $hashedContent = hash('md5', $post);
+
+        $encodeString = $method . "\n"
+                      . $hashedContent . "\n"
+                      . self::HEADER_CONTENT_TYPE . "\n"
+                      . $date . "\n"
+                      . $url . "\n";
+
+        $signature = base64_encode(hash_hmac('sha1', $encodeString, $this->authKey, false));
+
+        return [
+            'Date: ' . $date,
+            'Accept: ' . self::HEADER_ACCEPT,
+            'Content-Type: ' . self::HEADER_CONTENT_TYPE,
+            'x-pbx-authentication: '. $this->authKeyId . ':' . $signature,
+            'Content-MD5: ' . $hashedContent,
+        ];
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use IvanLemesehev\OnPBX\Client;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    const DOMAIN = 'domain';
+
+    const KEY = 'key';
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = new Client(self::DOMAIN, self::KEY);
+
+        parent::setUp();
+    }
+
+    public function testGet()
+    {
+        $result = $this->client->get('/history/search.json', []);
+
+        $this->assertEquals($result['status'], 0);
+        $this->assertEquals($result['comment'], 'not authenticated');
+    }
+
+    public function makeRequest()
+    {
+        $url = $result = $this->invokeMethod($this->client, 'getFullUrl', ['/history/search.json']);
+        $result = $result = $this->invokeMethod($this->client, 'makeRequest', ['POST', $url, [], []]);
+
+        $this->assertEquals($result['status'], 0);
+        $this->assertEquals($result['comment'], 'not authenticated');
+    }
+
+    public function testFullUrl()
+    {
+        $url = '/any_url';
+        $expected = 'api.onlinepbx.ru/' . self::DOMAIN . $url;
+        $result = $result = $this->invokeMethod($this->client, 'getFullUrl', [$url]);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testCreateHeaders()
+    {
+        $url = $result = $this->invokeMethod($this->client, 'getFullUrl', ['/history/search.json']);
+
+        $date = date('r');
+        $hashedContent = hash('md5', '123');
+
+        $encodeString = 'POST' . "\n"
+            . $hashedContent . "\n"
+            . 'application/x-www-form-urlencoded' . "\n"
+            . $date . "\n"
+            . $url . "\n";
+
+        $signature = base64_encode(hash_hmac('sha1', $encodeString, null, false));
+
+        $expected = [
+            'Date: ' . $date,
+            'Accept: application/json',
+            'Content-Type: application/x-www-form-urlencoded',
+            'x-pbx-authentication: ' . null . ':' . $signature,
+            'Content-MD5: ' . $hashedContent,
+        ];
+
+        $result = $result = $this->invokeMethod($this->client, 'createHeaders', [$url, '123', 'POST']);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    private function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+}
